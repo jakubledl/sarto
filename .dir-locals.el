0df878b887deda36ec8569f52671fab9da055629
Jakub Lédl <jakubledl@gmail.com>
2022-01-15 21:42:35 +0100

    fix(tools): get angular language server working

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+((typescript-mode . ((eval . (let ((probe-locations (concat (projectile-project-root) "/node_modules")))
+                               (setq
+                                 lsp-clients-angular-language-server-command
+                                 (list
+                                   "node"
+                                   (format "%s/@angular/language-server" probe-locations)
+                                   "--ngProbeLocations"
+                                   probe-locations
+                                   "--tsProbeLocations"
+                                   probe-locations
+                                   "--stdio")))))))
